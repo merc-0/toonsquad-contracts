0ab31c42673a99f4b43a6e7ceb4c0b79cc73b5ed
merc0 <merc-@mercdao.com>
2022-03-15 17:42:09 -0400

    pushing public

---
+++ src/test/Minter.t.sol
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.10;
+
+import "ds-test/test.sol";
+import "./mocks/MockCaller.sol";
+import "../BaseToken.sol";
+import "../Minter.sol";
+
+
+contract MinterTest is DSTest {
+
+    // contracts
+    BaseToken internal baseToken;
+    MockCaller internal mockCaller;
+    string internal name = "Test Token";
+    string internal symbol = "TT";
+
+
+    function setUp() public virtual {
+        baseToken = new BaseToken(name, symbol);
+        mockCaller = new MockCaller();
+
+        baseToken.grantRole(baseToken.MINTER_ROLE(), address(this));
+        baseToken.setMaxSupply(50);
+    }
+
+
+    /* ------------------------------- Permissions ------------------------------ */
+
+    function testPermission() public {
+        assertEq(baseToken.owner(), address(this));
+        assertTrue(baseToken.hasRole(baseToken.DEFAULT_ADMIN_ROLE(), address(this)));
+    }
+
+    function testSetBaseUri() public {
+        baseToken.mint(address(this));
+
+        baseToken.setBaseURI("something/");
+        assertEq(baseToken.tokenURI(1), "something/1");
+    }
+
+    function testSetTokenUri() public {
+        baseToken.mint(address(this));
+        baseToken.mint(address(this));
+        baseToken.setBaseURI("something/");
+        baseToken.setTokenURI(2, "anotherthing/2");
+        assertEq(baseToken.tokenURI(1), "something/1");
+        assertEq(baseToken.tokenURI(2), "anotherthing/2");
+    }
+
+    function testFailSetBaseUri() public {
+        baseToken.mint(address(this));
+        baseToken.mint(address(this));
+        mockCaller.setBaseUri(baseToken, "somethingnew");
+    }
+
+    function testFailSetTokenUri() public {
+        baseToken.mint(address(this));
+        baseToken.mint(address(this));
+        mockCaller.setTokenUri(baseToken, 1, "somethingnew");
+    }
+
+    function testFailSetMinter() public {
+        mockCaller.grantMinter(baseToken);
+    }
+
+    function testSetMinter() public {
+        baseToken.grantRole(baseToken.MINTER_ROLE(), address(mockCaller));
+        assertTrue(baseToken.hasRole(baseToken.MINTER_ROLE(), address(mockCaller)));
+    }
+
+
+    /* --------------------------------- Burning -------------------------------- */
+
+    function testBurn() public {
+        
+        // Setup.
+        baseToken.mint(address(this));
+        assertEq(baseToken.balanceOf(address(this)), 1);
+        assertEq(baseToken.totalSupply(), 1);
+        assertEq(baseToken.totalMinted(), 1);
+
+        // Burn.
+        baseToken.burn(1);
+        assertEq(baseToken.balanceOf(address(this)), 0);
+        assertEq(baseToken.totalSupply(), 0);
+        assertEq(baseToken.totalMinted(), 1);
+        assertEq(baseToken.totalBurned(), 1);
+    }
+
+    function testFailBurn() public {
+        baseToken.mint(address(this));
+        mockCaller.burnOne(baseToken, 1);
+    }
+
+
+    /* --------------------------------- Minting -------------------------------- */
+
+    function testFailMint() public {
+        mockCaller.mintOnToken(baseToken);
+    }
+
+    function testMint() public {
+        baseToken.mint(address(this));
+    }
+
+    function testCounters() public {
+        baseToken.mint(address(this));
+        assertEq(baseToken.balanceOf(address(this)), 1);
+        assertEq(baseToken.totalSupply(), 1);
+        assertEq(baseToken.totalMinted(), 1);
+    }
+
+    function testMaxMint() public {
+
+        for (uint256 index = 0; index < 50; index++) {
+            baseToken.mint(address(this));
+        }
+
+        assertEq(baseToken.balanceOf(address(this)), 50);
+        assertEq(baseToken.totalSupply(), 50);
+        assertEq(baseToken.totalMinted(), 50);
+    }
+
+    function testFailOverMaxMint() public {
+        for (uint256 index = 0; index < 51; index++) {
+            baseToken.mint(address(this));
+        }
+    }
+
+}
---
+++ src/test/mocks/MockCaller.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.10;
+
+import "../../BaseToken.sol";
+import "../../Minter.sol";
+
+contract MockCaller {
+
+    function setTokenUri(BaseToken tokenContact, uint256 tokenId, string calldata newTokenURI) public {
+        tokenContact.setTokenURI(tokenId, newTokenURI);
+    }
+
+    function setBaseUri(BaseToken tokenContact, string calldata newTokenURI) public {
+        tokenContact.setBaseURI(newTokenURI);
+    }
+
+    function burnOne(BaseToken tokenContact, uint256 id) public {
+        tokenContact.burn(id);
+    }
+
+
+
+    function grantMinter(BaseToken tokenContact) public {
+        tokenContact.grantRole(tokenContact.MINTER_ROLE(), address(this));
+    }
+
+    function mintOnToken(BaseToken tokenContact) public {
+        tokenContact.mint(address(this));
+    }
+
+    function mintOnMinter(Minter mintingContact, uint256 numberToMint) public {
+        mintingContact.mint(numberToMint);
+    }
+}
