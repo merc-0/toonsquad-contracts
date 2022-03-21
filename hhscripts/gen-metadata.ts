0b9b0f3f489cc898c9a27af5bb81feb29a97c36d
merc0 <merc-@mercdao.com>
2022-03-21 13:43:01 -0400

    sharing rotate

--- hhscripts/gen-metadata.ts
+++ hhscripts/gen-metadata.ts
@@ -1,8 +1,10 @@
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-ethers";
 import { Logger } from "tslog";const csv = require("csv-parser");
-import { writeFileSync, createReadStream, readFileSync } from 'fs';
+import { writeFileSync, readdirSync, readFileSync } from 'fs';
 import { ethers } from "ethers";
+import { strict as assert } from 'assert';
+
 
 const logger: Logger = new Logger();
 
@@ -26,6 +28,30 @@ const anotherRotate = (low: number, high: number, startingIdx: number) => {
         logger.info(`Metadata written to: ./provenance/meta/${idx}`);
 
         logger.info(`${idx} replaced with ${newIdx}`);
+    }
+}
+
+// Crude duplicate to change dirs.
+const rotate3 = (low: number, high: number, startingIdx: number) => {
+    
+    for (let index = low; index < high; index++) {
+        const fullRange = high - low;
+        const idx = index + 1;
+        // const offsetLocation = (idx - 1 + startingIndex - 1) % 10000;
+        const offsetLocation = (idx + (startingIdx - low)) % fullRange;
+        const newIdx = low + offsetLocation + 1;
+
+        // Read in
+        const fileString = readFileSync(`./provenance/metaRoundTwo/${newIdx}`, "utf-8");
+        const res = JSON.parse(fileString);
+
+        res.name = `#${idx}`;
+        
+        writeFileSync(`./provenance/metaRoundThree/${idx}`, JSON.stringify(res, null, 4));
+        logger.info(`Metadata written to: ./provenance/metaRoundThree/${idx}`);
+
+        logger.info(`${idx} replaced with ${newIdx}`);
+
     }
 }
 
@@ -72,6 +98,93 @@ task("another-rotate", "Next reveal increment")
     .setAction(
         async (args, hre) => {
             anotherRotate(parseInt(args.low) - 1, parseInt(args.high), parseInt(args.random));
+        }
+
+    );
+
+task("rotate-3", "Next reveal increment")
+    .addParam("high")
+    .addParam("low")
+    .addParam("random")
+    .setAction(
+        async (args, hre) => {
+            rotate3(parseInt(args.low) - 1, parseInt(args.high), parseInt(args.random));
+        }
+
+    );
+
+
+const getImgId = (s: string) => {
+    let parts = s.split("/");
+    parts =  parts[parts.length - 1].split(".");
+    return parseInt(parts[0]);
+}
+
+task("error-check")
+    .setAction(
+        async (args, hre) => {
+            let ids: number[] = [];
+            let imgIds: number[] = [];
+            let finalMeta = [];
+
+            const originalFiles = readdirSync(`./provenance/meta`);
+            const roundTwoFiles = readdirSync(`./provenance/metaRoundTwo`);
+            const roundThreeFiles = readdirSync(`./provenance/metaRoundThree`);
+
+            for (let index = 0; index < roundThreeFiles.length; index++) {
+                const element = roundThreeFiles[index];
+                if(element !== ".DS_Store") {
+
+                    const fileString = readFileSync(`./provenance/metaRoundThree/${element}`, "utf-8");
+                    const res = JSON.parse(fileString);
+                    finalMeta.push(res);
+    
+                    // logger.info(parseInt(element));
+                    // logger.info(res.image);
+                    // logger.info(getImgId(res.image));
+                    assert(!ids.includes(parseInt(element)));
+                    assert(!imgIds.includes(getImgId(res.image)));
+    
+                    ids.push(parseInt(element));
+                    imgIds.push(getImgId(res.image));
+                }
+            }
+
+            for (let index = 0; index < roundTwoFiles.length; index++) {
+                const element = roundTwoFiles[index];
+                if (!ids.includes(parseInt(element)) && element !== ".DS_Store") {
+                    const fileString = readFileSync(`./provenance/metaRoundTwo/${element}`, "utf-8");
+                    const res = JSON.parse(fileString);
+                    finalMeta.push(res);
+
+                    assert(!ids.includes(parseInt(element)));
+                    assert(!imgIds.includes(getImgId(res.image)));
+
+                    ids.push(parseInt(element));
+                    imgIds.push(getImgId(res.image));
+                }
+            }
+
+            for (let index = 0; index < originalFiles.length; index++) {
+                const element = originalFiles[index];
+                if (!ids.includes(parseInt(element)) && element !== ".DS_Store") {
+                    const fileString = readFileSync(`./provenance/meta/${element}`, "utf-8");
+
+                    const res = JSON.parse(fileString);
+                    finalMeta.push(res);
+
+                    assert(!ids.includes(parseInt(element)));
+                    assert(!imgIds.includes(getImgId(res.image)));
+
+                    ids.push(parseInt(element));
+                    imgIds.push(getImgId(res.image));
+                }
+            }
+
+            writeFileSync(`./provenance/ids`, JSON.stringify(ids, null, 4));
+            writeFileSync(`./provenance/finalMeta`, JSON.stringify(finalMeta, null, 4));
+            writeFileSync(`./provenance/imgIds`, JSON.stringify(imgIds, null, 4));
+
         }
 
     );
